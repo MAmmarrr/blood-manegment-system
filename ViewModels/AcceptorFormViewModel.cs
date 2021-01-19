77aa3a527608b1dfd731e749b0eedf40bdd1bef8
saifali25 <52177799+saifali25@users.noreply.github.com>
2021-01-19 07:23:20 +0500

    Adding Acceptor files

---
+++ ViewModels/AcceptorFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BloodBankManegmentSystem.Models;
+
+namespace BloodBankManegmentSystem.ViewModels
+{
+    public class AcceptorFormViewModel
+    {
+        public IEnumerable<BloodType> BloodTypes { get; set; }
+        public Acceptors Acceptor { get; set; }
+
+        public IEnumerable<Gender> Gender { get; set; }
+    }
+}
